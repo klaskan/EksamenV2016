e3dac4f5141799daea3dbed545e00d112bc76ded
Klas Rogne Kanestrøm <klas.kanestrom@gmail.com>
2018-01-10 14:48:59 +0100

    INFO132 Eksamen H2016

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * Oppgave 1
+         *
+         * a)
+         * Når vi kopilerer vil vi får følgende svar i terminalen:
+         * 1 + 2 = 12
+         *
+         * b)
+         * Når vi kopilerer vil vi får følgende svar i terminalen:
+         * 1 + 2 = 3
+         *
+         * c)
+         * Når vi kopilerer vil vi får følgende svar i terminalen:
+         * 12
+         *
+         * d)
+         * Når vi kopilerer vil vi får følgende svar i terminalen:
+         * 3
+         * 1
+         * 0
+         * 0
+         *
+         *
+         * e)
+         * Når vi kopilerer vil vi får følgende svar i terminalen:
+         * **********
+         * *********
+         * ********
+         * *******
+         * ******
+         * *****
+         * ****
+         * ***
+         * **
+         * *
+         *
+         *
+         *
+         */
+
+
+
+
+    }
+}
